eceda997322853b8d785eaf83a3c2bb02f960606
cristian <crissnar12xd@gmail.com>
2023-06-15 18:55:35 -0500

    si

--- js/app.js
+++ js/app.js
@@ -47,9 +47,43 @@
 // const elemento = document.querySelector('.si')
 // console.log (elemento)
 // elemento.style.display='none'
+   
 
+
+
+
+
+
+
+
+
+
+   
 
 // TRAVERSING
+
+// let elemento=document.querySelector('.contenedor-cards')
+// elemento.children[1].children[0].src="img/arriba.jpg"
+
+// CREACION DE ELEMENTOS
+
+// const elemento = document.createElement('A')
+// elemento.textContent="Mi cara cuando"
+// elemento.href=''
+
+// console.log(elemento)
+// elemento.onclick=funcionxd
+// function funcionxd(){
+//     alert("HOSTIA PUTA")
+// }
+
+// const nav = document.querySelector(".navegacion")
+// // nav.appendChild(elemento)
+// nav.insertBefore(elemento, nav.children[0])
+
+
+
+
 
 // let elemento = document.querySelector('.card')
 // console.log(elemento.children)
@@ -65,33 +99,49 @@
 // fondo.style.backgroundImage='../img/arriba2.jpg'
 // console.log(fondo)
 
-let elemento = parseInt(prompt("ingresa la opcion(1, 2, o 3)"))
-let navegar=document.querySelector('.navegacion')
-while (elemento !==1 || elemento !==2 || elemento!== 3){
-if (elemento===1){
-    document.querySelector(".si").textContent = "Bienvenido MotoViajero"
-    navegar.firstElementChild.textContent="Rutas"
-    navegar.children[1].textContent="Garaje"
-    navegar.children[2].textContent="Presupuesto"
-    navegar.lastElementChild.textContent=" "
-    break;
-}
-else if (elemento===2){
-    document.querySelector(".si").textContent = "Bienvenido motoposada "
-    navegar.firstElementChild.textContent="Ubicacion"
-    navegar.children[1].textContent="Calificacion"
-    navegar.children[2].textContent=" "
-    navegar.lastElementChild.textContent=" "
-    break;
+// let elemento = parseInt(prompt("ingresa la opcion(1, 2, o 3)"))
+// let navegar=document.querySelector('.navegacion')
+// while (elemento !==1 || elemento !==2 || elemento!== 3){
+// if (elemento===1){
+//     document.querySelector(".si").textContent = "Bienvenido MotoViajero"
+//     navegar.firstElementChild.textContent="Rutas"
+//     navegar.children[1].textContent="Garaje"
+//     navegar.children[2].textContent="Presupuesto"
+//     navegar.lastElementChild.textContent=" "
+//     break;
+// }
+// else if (elemento===2){
+//     document.querySelector(".si").textContent = "Bienvenido motoposada "
+//     navegar.firstElementChild.textContent="Ubicacion"
+//     navegar.children[1].textContent="Calificacion"   
+//     navegar.children[2].textContent=" "
+//     navegar.lastElementChild.textContent=" "
+//     break;
+// }
+
+
+// else if (elemento===3){
+//     document.querySelector(".si").textContent = "Bienvenido al mundo rutas para moteros"
+//     break;
+// }
+// else{
+//     alert("que")
+//     elemento = parseInt(prompt("ingresa la opcion(1, 2, o 3)"))
+// }
+// }
+
+
+const boton = document.querySelector('.btn-flotante')
+
+
+boton.addEventListener('click', funcion)
+
+function funcion (){
+    alert("o")
 }
 
 
-else if (elemento===3){
-    document.querySelector(".si").textContent = "Bienvenido al mundo rutas para moteros"
-    break;
-}
-else{
-    alert("que")
-    elemento = parseInt(prompt("ingresa la opcion(1, 2, o 3)"))
-}
-}
+
+
+
+
