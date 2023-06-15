777f634470e9500729476aaf03464de47c408a35
cristian <crissnar12xd@gmail.com>
2023-06-15 13:49:11 -0500

    primer commit

---
+++ js/app.js
@@ -0,0 +1,97 @@
+// let elemento = parseInt(prompt("ingresa la opcion(1, 2, o 3)"))
+
+// while (elemento !==1 || elemento !==2 || elemento!== 3){
+// if (elemento===1){
+//     document.querySelector(".si").textContent = "Bienvenido motero"
+//     break;
+// }
+// else if (elemento===2){
+//     document.querySelector(".si").textContent = "Bienvenido motoposada "
+//     break;
+// }
+
+
+// else if (elemento===3){
+//     document.querySelector(".si").textContent = "Bienvenido al mundo rutas para moteros"
+//     break;
+// }
+// else{
+//     alert("que")
+//     elemento = parseInt(prompt("ingresa la opcion(1, 2, o 3)"))
+// }
+// }
+
+
+
+
+// let element = document.images
+// console.log(element)
+
+// const encabezado=document.getElementsByClassName('section')
+// console.log(encabezado)
+
+// let elemento=document.querySelectorAll(prompt("que clase deseas buscar"))
+// console.log(elemento)
+
+// let elemento=document.querySelector('.contenedor-hero').textContent=prompt("si")
+
+
+// cambiar imagen
+
+// let elemento = document.querySelector('.card img')
+// elemento.src="/img/arriba.jpg"
+// console.log(elemento)
+
+
+
+// const elemento = document.querySelector('.si')
+// console.log (elemento)
+// elemento.style.display='none'
+
+
+// TRAVERSING
+
+// let elemento = document.querySelector('.card')
+// console.log(elemento.children)
+// console.log(elemento.children [1])
+// elemento.children[1].children[1].textContent="si"
+
+
+
+// let elemento = document.querySelector('.navegacion')
+
+
+// let fondo = document.querySelector('.hero')
+// fondo.style.backgroundImage='../img/arriba2.jpg'
+// console.log(fondo)
+
+let elemento = parseInt(prompt("ingresa la opcion(1, 2, o 3)"))
+let navegar=document.querySelector('.navegacion')
+while (elemento !==1 || elemento !==2 || elemento!== 3){
+if (elemento===1){
+    document.querySelector(".si").textContent = "Bienvenido MotoViajero"
+    navegar.firstElementChild.textContent="Rutas"
+    navegar.children[1].textContent="Garaje"
+    navegar.children[2].textContent="Presupuesto"
+    navegar.lastElementChild.textContent=" "
+    break;
+}
+else if (elemento===2){
+    document.querySelector(".si").textContent = "Bienvenido motoposada "
+    navegar.firstElementChild.textContent="Ubicacion"
+    navegar.children[1].textContent="Calificacion"
+    navegar.children[2].textContent=" "
+    navegar.lastElementChild.textContent=" "
+    break;
+}
+
+
+else if (elemento===3){
+    document.querySelector(".si").textContent = "Bienvenido al mundo rutas para moteros"
+    break;
+}
+else{
+    alert("que")
+    elemento = parseInt(prompt("ingresa la opcion(1, 2, o 3)"))
+}
+}
